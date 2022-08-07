c30865aaa9e813e53f0847eeeac2abfa0bb127c2
Matt Traudt <sirmatt@ksu.edu>
2022-08-07 08:55:05 -0400

    Build HashMap that stores loop begin and ends
    This is a performance regression

--- src/bin/bf.rs
+++ src/bin/bf.rs
@@ -2,9 +2,35 @@ use brainfuck::instruction::*;
 use brainfuck::pseudo_instruction::PseudoInst;
 use brainfuck::tape::Tape;
 use brainfuck::EOFAction;
+use std::collections::HashMap;
 use std::default::Default;
 use std::fs::File;
 use std::io::{self, Read, Write};
+
+fn begin_end_pairs(insts: &[PseudoInst]) -> HashMap<usize, usize> {
+    let n_begins = insts
+        .iter()
+        .filter(|&&i| i == PseudoInst::Plain(Inst::Begin))
+        .count();
+    let mut begin_stack = Vec::with_capacity(n_begins/2);
+    let mut map = HashMap::with_capacity(n_begins);
+    for (idx, pi) in insts.iter().enumerate() {
+        match pi {
+            PseudoInst::Plain(i) => match i {
+                Inst::Begin => begin_stack.push(idx),
+                Inst::End => {
+                    let b = begin_stack.pop().expect("Unbalance [ and ] in code");
+                    let e = idx;
+                    map.insert(b, e);
+                }
+                _ => continue,
+            },
+            _ => continue,
+        }
+    }
+    println!("be map has {} keys", map.len());
+    map
+}
 
 fn main() -> io::Result<()> {
     let args = std::env::args().collect::<Vec<_>>();
@@ -19,6 +45,7 @@ fn main() -> io::Result<()> {
     let stdin = io::stdin();
     let mut input_bytes = stdin.lock().bytes();
     let code = Vec::from(InstIter::new(Box::new(File::open(code_fname)?)));
+    let begin_end = begin_end_pairs(&code);
     let mut code_ptr = 0;
     while code_ptr < code.len() {
         let pi = &code[code_ptr];
@@ -34,18 +61,7 @@ fn main() -> io::Result<()> {
                     if tape.get() != 0 {
                         loop_starts.push(code_ptr);
                     } else {
-                        let mut ends_needed = 1;
-                        loop {
-                            code_ptr += 1;
-                            if code[code_ptr] == PseudoInst::Plain(Inst::Begin) {
-                                ends_needed += 1;
-                            } else if code[code_ptr] == PseudoInst::Plain(Inst::End) {
-                                ends_needed -= 1;
-                            }
-                            if ends_needed == 0 {
-                                break;
-                            }
-                        }
+                        code_ptr = begin_end[&code_ptr];
                         assert_eq!(code[code_ptr], PseudoInst::Plain(Inst::End));
                     }
                 }
